ea61a0352bdbd49411ca2de71faa6bed7d0119bb
OlirEswar <92946878+OlirEswar@users.noreply.github.com>
2023-12-04 15:49:20 -0500

    Added components for about and work

---
+++ src/About.jsx
@@ -0,0 +1,17 @@
+import "./About.css";
+
+function About() {
+  return (
+    <div className="AboutComponentWrapper">
+      <h2>Olir Eswar</h2>
+      <p>
+        I'm an Engineering student at Dartmouth College and a Computer Science
+        student at Vassar College. I love bringing ideas into life through both
+        software and hardware. I have experience in full-stack web development,
+        machine learning, and computer hardware.
+      </p>
+    </div>
+  );
+}
+
+export default About;
