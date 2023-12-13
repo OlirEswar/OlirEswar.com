05b3afb4efbfe8947d65257862086c2e7e823713
OlirEswar <92946878+OlirEswar@users.noreply.github.com>
2023-12-13 04:15:24 -0500

    Changed Bio slightly, made sure index styling is the same

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <div className="left-sibling"></div>
     <div className="main-container">
       <ProfileCard />
       <Bio />
