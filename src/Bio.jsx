710daa7b134c6546b17c56e90d157880f1bbdd90
OlirEswar <92946878+OlirEswar@users.noreply.github.com>
2023-12-13 00:35:58 -0500

    Added styles to bio

--- src/Bio.jsx
+++ src/Bio.jsx
@@ -2,8 +2,18 @@ import "./Bio.css";
 
 function Bio() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="bio">
+      <h1>I'm Olir</h1>
+      <h2>Comp Sci at Vassar</h2>
+      <div className="bio__buttons">
+        <button id="project-button">Projects</button>
+        <button id="resume-button">Resume</button>
+      </div>
+      <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex{" "}
+      </p>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import ProfileCard from "./ProfileCard.jsx";
-//import "./index.css";
+import Bio from "./Bio.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ProfileCard />
+    <div className="main-container">
+      <ProfileCard />
+      <Bio />
+    </div>
   </React.StrictMode>
 );
