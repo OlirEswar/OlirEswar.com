e7e0d10eb4bfa3e7f2373f09edcfb19bf2950b1c
OlirEswar <92946878+OlirEswar@users.noreply.github.com>
2023-12-16 02:16:29 -0500

    Fixed bug where contact p style applied to all p's

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -4,11 +4,15 @@ function Contact() {
   return (
     <div className="contact-container">
       <div className="contact-container__info">
-        <h1>Phone</h1>
-        <h2>(516) 287-8286</h2>
         <h1>Social Media</h1>
         <h2>Linkedin: Oliravan Eswaramoorthy</h2>
         <h2>Github: OlirEswar</h2>
+        <p>
+          Thank you for taking the time to look through my website. Whether you
+          are looking for a software developer, hoping to collaborate on a
+          project, or just want to meet somebody new, I would love to talk with
+          you!
+        </p>
       </div>
       <div className="contact-container__form"></div>
     </div>
