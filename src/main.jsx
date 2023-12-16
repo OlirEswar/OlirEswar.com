ac294b2a89402a59bcf80a8a0d4a0edb16f3b41f
OlirEswar <92946878+OlirEswar@users.noreply.github.com>
2023-12-16 06:47:05 -0500

    Added new project cards

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,10 @@ import { BrowserRouter } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
 import Contact from "./Contact.jsx";
 import ursi_poster_img from "/URSI_Poster_Full.png";
+import personal_website_img from "/personal_website.png";
+import vsa_website from "/VSA_website.png";
+import FPGA_conway from "/FPGA_conway.png";
+import HSRP_img from "/HSRP_poster.png";
 import ContactForm from "./ContactForm.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -40,6 +44,28 @@ ReactDOM.createRoot(document.getElementById("root")).render(
               <h1>Projects</h1>
               <ProjectCard
                 isOdd={true}
+                title="Personal Website"
+                description="This is the website that you are on right now! I made this website to showcase the
+                different projects that I've done through the years and practice my frontend development skills.
+                I designed and built the frontend using React and styled the website using CSS."
+                languages={["HTML", "CSS", "React"]}
+                image={personal_website_img}
+                button_name="Github"
+                link="https://www.youtube.com/watch?v=ayG11VflF50"
+              />
+              <ProjectCard
+                isOdd={false}
+                title="FPGA Game of Life"
+                description="I worked with a friend to implement Conway's Game of Life on an FPGA. We used VHDL
+                to program our hardware, so that it would calculate the appropriate state and display the pattern
+                on a VGA screen. This project was made for ENGS 31: Digital Electronics at Dartmouth."
+                languages={["VHDL", "FPGA", "Vivado"]}
+                image={FPGA_conway}
+                button_name="Demo Video"
+                link="https://www.youtube.com/watch?v=ayG11VflF50"
+              />
+              <ProjectCard
+                isOdd={true}
                 title="Eye-Tracking AI for Cognitive Science"
                 description="This project was made for the Undergraduate Research Summer Institute in 2021 
                 in collaboration with Dr. de Leeuw, a Cognitive Science Professor at Vassar College, and
@@ -56,10 +82,29 @@ ReactDOM.createRoot(document.getElementById("root")).render(
               />
               <ProjectCard
                 isOdd={false}
-                title="Cognitive Science Eye-Tracking"
-                description="blah"
-                languages={["blah", "blah2", "blah3"]}
-                image={ursi_poster_img}
+                title="Vassar Student Association Website"
+                description="I was the web development intern at the Vassar Student Association during my freshman year.
+                I was tasked with updating the website whenever new information, like new legislation, events, or officials
+                were recognized. I worked on creating new pages for different organizations on campus and created a digital archive
+                for committee meeting minutes. I also helped develop a  casework system, so that students could confidentially 
+                direct concerns to senators via the website. I used wordpress, HTML, CSS, and Javascript to implement the features we needed."
+                languages={["Wordpress", "Javascript", "CSS"]}
+                image={vsa_website}
+                button_name="View Site"
+                link="https://vsa.vassar.edu/"
+              />
+              <ProjectCard
+                isOdd={true}
+                title="Docker Containers for Physics Experiments"
+                description="I worked at the Brookhaven National Lab to create Linux operating system environment container for the
+                Relativistic Heavy Ion Collider project. This was done to ensure that the software controlling the experiments could
+                be easily migrated from Scientific Linux 7 to CentOS 8 without affecting its functionality and causing errors. During
+                the internship I found replacements for packages that did not have CentOS 8 versions and wrote custom scripts to deploy
+                the containers on the high-performance cluster."
+                languages={["Linux", "Docker", "RPM"]}
+                image={HSRP_img}
+                button_name="Poster"
+                link="https://drive.google.com/drive/folders/1p-ir201LI5uP1NtnqM1oqP6Dh-R_k7-b"
               />
             </div>
           }
