f15ea4df29b87d99d2dce18320cd1136d210e26d
OlirEswar <92946878+OlirEswar@users.noreply.github.com>
2023-12-16 02:42:34 -0500

    Changed contact message

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -8,10 +8,10 @@ function Contact() {
         <h2>Linkedin: Oliravan Eswaramoorthy</h2>
         <h2>Github: OlirEswar</h2>
         <p>
-          Thank you for taking the time to look through my website. Whether you
-          are looking for a software developer, hoping to collaborate on a
-          project, or just want to meet somebody new, I would love to talk with
-          you!
+          Thank you for taking the time to look through my website. If you
+          enjoyed your time here, please consider reaching out. Whether you are
+          looking for a software developer, hoping to collaborate on a project,
+          or just want to meet somebody new, I would love to talk with you!
         </p>
       </div>
       <div className="contact-container__form"></div>
